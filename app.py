3dae90e95dabe271ed19f75cc32b4f35b77ac46c
gudween.code <gudween.code@gmail.com>
2022-01-05 17:57:57 +0300

    Fixing Heroku deployment bugs

--- app.py
+++ app.py
@@ -6,18 +6,12 @@ from security import authenticate, identity
 from resources.user import UserRegister
 from resources.item import Item, Items
 from resources.store import Store, StoreList
-from db import db
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///data.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.secret_key = 'jose'
 api = Api(app)
-
-@app.before_first_request
-def create_tables():
-    db.create_all()
-
 
 jwt = JWT(app, authenticate, identity)
 
@@ -38,5 +32,4 @@ def hello():
     return r.text
 
 if __name__ == '__main__':
-    db.init_app(app)
     app.run(port=5000, debug=True)
